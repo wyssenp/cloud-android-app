c259572ede713fe4c0342f902186a87e5eaec3b0
Pierre-Alain Wyssen <pierry_wyssen@hotmail.com>
2015-01-09 10:12:49 +0100

    Update LeaderboardsRegistration.java

--- Technopole_MyTracks/src/ch/technotracks/ui/LeaderboardsRegistration.java
+++ Technopole_MyTracks/src/ch/technotracks/ui/LeaderboardsRegistration.java
@@ -8,14 +8,10 @@ import android.os.Bundle;
 
 public class LeaderboardsRegistration extends FragmentActivity {
 
-	//This is a comment
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_leaderboards_registration);
-
-//		Button btnContinue = (Button) findViewById(R.id.button1);
-//		Button btnDoItLater = (Button) findViewById(R.id.button2);
 	}
 
 	public void backToMain(View view) {
